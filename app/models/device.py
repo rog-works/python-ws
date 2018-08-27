744ebffe701f5cc669b61771f8977e09a7267568
rog-works <rog_works@yahoo.co.jp>
2018-08-27 14:09:44 +0900

    feat: update model dependencies

--- app/models/device.py
+++ app/models/device.py
@@ -3,4 +3,35 @@
 from app.models.model import Model
 
 class Device(Model):
-	pass
+    @classmethod
+    def create(cls, data):
+        return cls.api().AddDevice(data)
+
+    @classmethod
+    def get(cls, id: str):
+        return cls(cls.api().GetDevice())
+
+    @classmethod
+    def all(cls) -> list:
+        return cls.api().GetAllDevices().map(lambda row: cls(row))
+
+    @classmethod
+    def update(cls, data: dict):
+        cls.api().SetDevice(data)
+
+    @classmethod
+    def delete(cls, primary_id):
+        cls.api().DeleteDevice(primary_id)
+
+    def on(self):
+        self.__class__.update(self.to_on_data())
+
+    def off(self):
+        self.__class__.update(self.to_off_data())
+
+    def bright(self, power: int):
+        self.__class__.update(self.to_bright_data())
+
+    def volume(self, power: int):
+        self.__class__.update(self.to_volume_data())
+
--- app/models/model.py
+++ app/models/model.py
@@ -4,36 +4,6 @@ from di.container import Container as DI
 
 class Model(object):
 	@classmethod
-	def create(cls, data: dict):
-		return DI.instance.get('db').create(data)
+	def api(cls):
+		return DI.instance.get('api')
 
-	@classmethod
-	def get(cls, primary_id):
-		return cls(DI.instance.get('db').get(primary_id))
-
-	@classmethod
-	def find(cls, page: int = 1, limit: int = 10) -> list:
-		for data in DI.instance.get('db').find(page, limit):
-			rows.append(cls(data))
-		return rows
-
-	@classmethod
-	def update(cls, data: dict):
-		DI.instance.get('db').update(data)
-
-	@classmethod
-	def delete(cls, primary_id):
-		DI.instance.get('db').delete(primary_id)
-
-	@property
-	def primary_id(self):
-		assert True, 'Not implemented property.'
-
-	def save(self):
-		if not self.primary_id:
-			self.primary_id = self.__class__.create(self.to_dict())
-		else:
-			self.__class__.update(self.to_dict())
-
-	def remove(self):
-		self.__class__.delete(self.primary_id)
