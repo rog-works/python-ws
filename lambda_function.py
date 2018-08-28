f762eb63390b1e60d2c79ab09b02b7dff56d32dc
rog-works <rog_works@yahoo.co.jp>
2018-08-28 19:20:12 +0900

    fix: add path cwd

--- lambda_function.py
+++ lambda_function.py
@@ -1,7 +1,12 @@
 # -*- coding: utf-8 -*-
 
+import os
+import sys
+
+sys.path.append(os.getcwd())
+
 from data.config import Config
 from app.awslambda import AwsLambda as App
 
-def lambda_function(event, context):
+def lambda_handler(event, context):
 	return App().run(Config('config/app-dev.yml'), event)
