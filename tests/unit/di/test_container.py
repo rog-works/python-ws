28938a9338668a7d286a63935ea561f01dbdf68f
rog-works <rog_works@yahoo.co.jp>
2018-08-30 00:00:18 +0900

    test: add inject testing

--- tests/unit/di/test_container.py
+++ tests/unit/di/test_container.py
@@ -6,10 +6,40 @@ from di.di import register, inject
 class TestContainer(unittest.TestCase):
 	def test_register_and_inject(self):
 		register('hoge', lambda : 123)
+		register('fuga', lambda : '456')
+		register('piyo', lambda : [789])
 
 		@inject('hoge')
-		def inner_func(hoge):
+		def injectx1_func(hoge):
 			return hoge
 
-		self.assertEqual(123, inner_func())
+		@inject('fuga')
+		def injectx1_arg_func(arg, fuga):
+			return arg, fuga
+
+		@inject('fuga', 'piyo')
+		def injectx2_arg_func(arg, fuga, piyo):
+			return arg, fuga, piyo
+
+		class InnerClass(object):
+			@inject('hoge')
+			def __init__(self, hoge):
+				self.hoge = hoge
+
+			@inject('fuga')
+			def injectx1_arg_func(self, arg, fuga):
+				return arg, fuga
+
+			@inject('piyo')
+			@classmethod
+			def injectx1_argx2_func(cls, arg1, arg2, piyo):
+				return arg1, arg2, piyo
+
+		self.assertEqual(123, injectx1_func())
+		self.assertEqual(('arg1', '456'), injectx1_arg_func('arg1'))
+		self.assertEqual(('arg1', '456', [789]), injectx2_arg_func('arg1'))
+		self.assertEqual(123, InnerClass().hoge)
+		self.assertEqual(('arg1', '456'), InnerClass().injectx1_arg_func('arg1'))
+		with self.assertRaises(TypeError):
+			InnerClass.injectx1_argx2_func('hoge', 'fuga')
 
