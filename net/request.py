1e8a38a865ead14f6c7bd89649dfa8cb5ecc66e9
rog-works <rog_works@yahoo.co.jp>
2018-09-01 18:29:08 +0900

    feat: Request add method

--- net/request.py
+++ net/request.py
@@ -3,16 +3,17 @@
 class Request(object):
 	"""リクエスト情報の管理"""
 
-	def __init__(self, url: str, headers: dict, queries: dict, body: dict):
+	def __init__(self, url: str, method: str, headers: dict, queries: dict, body: dict):
 		"""インスタンスを生成
 
-		Args:
-			url: URL
-			headers: ヘッダー
-			queries: クエリー
-			body: リクエストボディ
+		:param str url: URL
+		:param str method: メソッド
+		:param str headers: ヘッダー
+		:param str queries: クエリー
+		:param str body: リクエストボディ
 		"""
 		self._url = url
+		self._method = method
 		self._headers = headers
 		self._queries = queries
 		self._body = body
@@ -22,43 +23,40 @@ class Request(object):
 		"""URLを取得するプロパティ"""
 		return self._url
 
+	@property
+	def method(self) -> str:
+		"""メソッドを取得するプロパティ"""
+		return self._method
+
 	def has_header(self, key: str) -> bool:
 		"""指定のヘッダーが存在するか判定
 
-		Returns:
-			True = 存在
+		:return bool: True = 存在
 		"""
 		return key in self._headers
 
-	def header(self, key: str, default = None):
-		"""指定のヘッダー値を取得
+	def header(self, key: str, default = None) -> str:
+		"""指定のヘッダー値を取得。キーが存在しない場合はデフォルト値を返却
 
-		Args:
-			key: クエリキー
-			default: デフォルト値
-
-		Returns:
-			ヘッダー値
+		:param str key: ヘッダーキー
+		:param any default: デフォルト値
+		:return str: ヘッダー値
 		"""
 		return self._headers[key] if key in self._headers else default
 
 	def has_query(self, key: str) -> bool:
 		"""指定のクエリーが存在するか判定
 
-		Returns:
-			True = 存在
+		:return bool: True = 存在
 		"""
 		return key in self._queries
 
-	def query(self, key: str, default = None):
-		"""指定のクエリー値を取得
+	def query(self, key: str, default = None) -> str:
+		"""指定のクエリー値を取得。キーが存在しない場合はデフォルト値を返却
 
-		Args:
-			key: クエリーキー
-			default: デフォルト値
-
-		Returns:
-			クエリー値
+		:param str key: クエリーキー
+		:param any default: デフォルト値
+		:return str: クエリー値
 		"""
 		return self._queries[key] if key in self._queries else default
 
@@ -73,6 +71,7 @@ class Builder(object):
 	def __init__(self):
 		"""インスタンスを生成"""
 		self._url = ''
+		self._method = ''
 		self._headers = {}
 		self._queries = {}
 		self._body = {}
@@ -80,57 +79,57 @@ class Builder(object):
 	def url(self, url: str):
 		"""URLを設定
 
-		Args:
-			url: URL
+		:param str url: URL
 		"""
 		self._url = url
+
+	def method(self, method: str):
+		"""メソッドを設定
+
+		:param str method: メソッド
+		"""
+		self._method = method
 
 	def headers(self, headers: dict):
 		"""ヘッダーを設定
 
-		Args:
-			headers: ヘッダー
+		:param dict headers: ヘッダー
 		"""
 		self._headers = headers
 
-	def add_header(self, key: str, value):
+	def add_header(self, key: str, value: str):
 		"""ヘッダーを追加
 
-		Args:
-			key: ヘッダーキー
-			value: 値
+		:param str key: ヘッダーキー
+		:param str value: 値
 		"""
 		self._headers[key] = value
 
 	def queries(self, queries: dict):
 		"""クエリーを設定
 
-		Args:
-			queries: クエリー
+		:param dict queries: クエリー
 		"""
 		self._queries = queries
 
-	def add_query(self, key: str, value):
+	def add_query(self, key: str, value: str):
 		"""クエリーを追加
 
-		Args:
-			key: クエリキー
-			value: 値
+		:param str key: クエリーキー
+		:param str value: 値
 		"""
 		self._queries[key] = value
 
 	def body(self, body: dict):
 		"""リクエストボディを設定
 
-		Args:
-			body: リクエストボディ
+		:param dict body: リクエストボディ
 		"""
 		self._body = body
 
 	def build(self) -> Request:
 		"""リクエストを生成
 
-		Returns:
-			リクエスト
+		:return Request: リクエスト
 		"""
-		return Request(self._url, self._headers, self._queries, self._body)
+		return Request(self._url, self._method, self._headers, self._queries, self._body)
