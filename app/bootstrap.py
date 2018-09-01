f0ed65888f24d8689709f16e4bf3323ab2065ac5
rog-works <rog_works@yahoo.co.jp>
2018-09-01 19:58:18 +0900

    docs: bootstrap on doc string

--- app/bootstrap.py
+++ app/bootstrap.py
@@ -6,10 +6,23 @@ from net.request import Request
 from di.di import register
 
 class Bootstrap(object):
+	"""起動時の処理をまとめる"""
+
 	def __init__(self, config: Config, request: Request):
+		"""インスタンスを生成
+
+		:param Config config: コンフィグ
+		:param Request request: リクエスト
+		"""
 		self.__di_register(config, request)
 
 	def __di_register(self, config: Config, request: Request):
+		"""DIコンテナーへの登録
+
+		:param Config config: コンフィグ
+		:param Request request: リクエスト
+		:raise DataFormatError: コンフィグ内のDI定義が不正
+		"""
 		register('config', lambda : config)
 		register('request', lambda : request)
 
@@ -27,5 +40,11 @@ class Bootstrap(object):
 				raise DataFormatError(f'Unexpected di deffinition. key = {key}, deffinition = {deffinition}')
 
 	def __lazy_load_module(self, path: str, module: str, *args):
-		return lambda : getattr(import_module(path), module)(*args)
+		"""モジュールファクトリーの生成
 
+		:param str path: モジュールパス
+		:param str module: モジュール名
+		:param tuple *args: モジュールへの引数
+		:return function: モジュールファクトリー
+		"""
+		return lambda : getattr(import_module(path), module)(*args)
